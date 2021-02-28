8ed018228371bed0a32b87a5134fb89fe8fe0c9b
Takeshi Ikuma <tikuma@gmail.com>
2021-02-28 13:43:18 -0600

    Fixed docstring fomformance

--- src/parselmouth/LPC_docstrings.h
+++ src/parselmouth/LPC_docstrings.h
@@ -11,7 +11,7 @@ a frame of acoustic data samples.
 
 Note that the $a_0=1$ coefficient is omitted in a.
 
-This class is instantiated in :object:`~parselmouth.LPC` object and not 
+This class is instantiated in :object:`~parselmouth.LPC` object and not
 intended to be instantiated by user.
 
 See also
@@ -27,15 +27,15 @@ constexpr auto FRAME_A = R"(numpy.ndarray of float : Linear predictive model coe
 
 constexpr auto CLASS_DOCSTRING = R"(Praat LPC Class.
 
-A sequence object contains the outcomes of linear predictive coding (LPC) 
+A sequence object contains the outcomes of linear predictive coding (LPC)
 analysis outcomes. The analysis is performed repeatedly with a sliding
-window, which offsets :attr:`LPC.dt<parselmouth.LPC.dt>` seconds between 
+window, which offsets :attr:`LPC.dt<parselmouth.LPC.dt>` seconds between
 frames.
 
 The i-th item of this object is a :func:`LPC.Frame<parselmouth.LPC.Frame>`
 object representing the i-th frame.
 
-In the LPC analysis one tries to predict xn on the basis of the p previous 
+In the LPC analysis one tries to predict xn on the basis of the p previous
 samples, x′n = ∑ ak xn-k then {a1, a2, ..., ap} can be chosen to minimize
 the prediction power Qp where Qp = E[ |xn - x′n|2 ].
 
@@ -58,14 +58,14 @@ constexpr auto MAX_N_COEFFICIENTS_DOCSTRING = R"(Largest number of coefficients 
 
 constexpr auto TO_LINE_SPECTRAL_FREQUENCIES=R"(Convert to line spectra.
 
-Returns :object:`parselmouth.LineSpectralFrequencies` object with the 
+Returns :object:`parselmouth.LineSpectralFrequencies` object with the
 line frequencies found in the LPC models.
 
 Parameter
 ----------
 grid_size : float, default=0.0
     TBD
-    
+
 See also
 --------
 :object:`parselmouth.LineSpectralFrequencies`
@@ -77,7 +77,7 @@ constexpr auto TO_SPECTRUM_SLICE_DOCSTRING=R"(Convert to spectrogram.
 Returns :object:`parselmouth.Spectrum` object with the spectral
 representation of the LPC model found at specified time t.
 
-The Spectrum at t will be calculated from the nearest 
+The Spectrum at t will be calculated from the nearest
 :object:`~parselmouth.LPC.Frame`. See :praat:`LPC: To Spectrum (slice)...`
 for dedailed algorithm description.
 
@@ -85,24 +85,24 @@ Parameters
 ----------
 time : float
     Time at which the spectrum should be calculated.
-    
+
 minimum_frequency_resolution : float, default=20.0
     Maximum distance separation of successive frequencies in the Spectrum,
     in Hz
 
 bandwidth_reduction : float, default=0.0
-    Reduces the bandwidth of each zero by this factor (<=0.0 for no 
-    reduction). Formants with small bandwidths show up very well as darker 
-    regions in the spectrogram because the poles lie close to the contour 
-    along which a spectrum is computed (the unit circle in the z-plane). 
+    Reduces the bandwidth of each zero by this factor (<=0.0 for no
+    reduction). Formants with small bandwidths show up very well as darker
+    regions in the spectrogram because the poles lie close to the contour
+    along which a spectrum is computed (the unit circle in the z-plane).
     Peak enhancement can be realized by computing a spectrum in the z-plane
     along a contour of radius:
 
-        r = exp (– pi x bandwidthReduction / samplingFrequency). 
+        r = exp (– pi x bandwidthReduction / samplingFrequency).
 
 deemphasis_frequency : float, default=50.0
     Performs de-emphasis when value is in this interval, specified in Hz.
-    (0, Nyquist frequency) 
+    (0, Nyquist frequency)
 
 See also
 --------
@@ -114,7 +114,7 @@ constexpr auto TO_SPECTROGRAM_DOCSTRING=R"(Convert to spectrogram.
 Returns :object:`parselmouth.Spectrogram` object with the spectral
 representation of the LPC models.
 
-For each LPC_Frame the corresponding Spectrum will be calculated according 
+For each LPC_Frame the corresponding Spectrum will be calculated according
 to the algorithm explained in :func:`parselmouth.LPC.to_spectrum`. For each
 frequency the power, i.e., the square of the complex values, will be stored
 in the corresponding area in the Spectrogram.
@@ -126,18 +126,18 @@ minimum_frequency_resolution : float, default=20.0
     in Hz
 
 bandwidth_reduction : float, default=0.0
-    Reduces the bandwidth of each zero by this factor (<=0.0 for no 
-    reduction). Formants with small bandwidths show up very well as darker 
-    regions in the spectrogram because the poles lie close to the contour 
-    along which a spectrum is computed (the unit circle in the z-plane). 
+    Reduces the bandwidth of each zero by this factor (<=0.0 for no
+    reduction). Formants with small bandwidths show up very well as darker
+    regions in the spectrogram because the poles lie close to the contour
+    along which a spectrum is computed (the unit circle in the z-plane).
     Peak enhancement can be realized by computing a spectrum in the z-plane
     along a contour of radius:
 
-        r = exp (– pi x bandwidthReduction / samplingFrequency). 
+        r = exp (– pi x bandwidthReduction / samplingFrequency).
 
 deemphasis_frequency : float, default=50.0
     Performs de-emphasis when value is in this interval, specified in Hz.
-    (0, Nyquist frequency) 
+    (0, Nyquist frequency)
 
 See also
 --------
